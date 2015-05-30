0aa4655187f33cfe21df3e5f2beb5f225d87cc31
Koichi ITO <koic.ito@gmail.com>
2015-05-31 01:48:28 +0900

    separate setup test data

--- spec/active_pstore/base_spec.rb
+++ spec/active_pstore/base_spec.rb
@@ -3,21 +3,7 @@ require 'active_pstore'
 require 'date'
 
 describe ActivePStore::Base do
-  class Artist < ActivePStore::Base
-    def initialize(name, associated_act, instrument, birth_date)
-      @name = name
-      @associated_act = associated_act
-      @instrument = instrument
-      @birth_date = birth_date
-    end
-
-    attr_accessor :name, :associated_act, :instrument, :birth_date
-  end
-
-  let(:randy_rhoads)  { Artist.new('Randy Rhoads', 'Ozzy Osbourne', 'guitar', Date.new(1956, 12, 6)) }
-  let(:michael_amott) { Artist.new('Michael Amott', 'Arch Enemy', 'guitar', Date.new(1969, 7, 28)) }
-  let(:don_airey)     { Artist.new('Don Airey', 'Ozzy Osbourne', 'keyboard', Date.new(1948, 6, 21)) }
-  let(:zakk_wylde)    { Artist.new('Zakk Wylde', 'Ozzy Osbourne', 'guitar', Date.new(1948, 12, 3)) }
+  include_context 'defined artists'
 
   before do
     Artist.establish_connection(database: '/tmp/active_pstore_test')
