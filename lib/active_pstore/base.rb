9f55cd9cf893eea547d55f9a9ef54e14f28c715f
Koichi ITO <koic.ito@gmail.com>
2015-05-31 20:49:08 +0900

    refactoring

--- lib/active_pstore/base.rb
+++ lib/active_pstore/base.rb
@@ -3,6 +3,7 @@ module ActivePStore
     extend ActivePStore::ConnectionHandling
     extend ActivePStore::FinderMethods
     extend ActivePStore::Inheritance
+    extend ActivePStore::QueryMethods
     include ActivePStore::Persistence
 
     class << self
@@ -10,24 +11,6 @@ module ActivePStore
         use_connection do |connection|
           connection[self.pstore_key] || []
         end
-      end
-
-      def where(conditions = {})
-        ret = all
-
-        return ret if conditions.empty?
-
-        conditions.each do |key, value|
-          ret = ret.select {|obj|
-            if value.is_a? Range
-              value.include?(obj.__send__(key))
-            else
-              obj.__send__(key) == value
-            end
-          }
-        end
-
-        ret
       end
 
       def update_all(attrs)
---
+++ lib/active_pstore/query_methods.rb
@@ -0,0 +1,21 @@
+module ActivePStore
+  module QueryMethods
+    def where(conditions = {})
+      ret = all
+
+      return ret if conditions.empty?
+
+      conditions.each do |key, value|
+        ret = ret.select {|obj|
+        if value.is_a? Range
+          value.include?(obj.__send__(key))
+        else
+          obj.__send__(key) == value
+        end
+        }
+      end
+
+      ret
+    end
+  end
+end
