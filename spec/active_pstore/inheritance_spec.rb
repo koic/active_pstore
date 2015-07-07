9601f96c20d1c8fb20e3fda1c7ac80bbd652d8a9
Koichi ITO <koic.ito@gmail.com>
2015-07-08 00:00:00 +0900

    :lipstick:

--- spec/active_pstore/inheritance_spec.rb
+++ spec/active_pstore/inheritance_spec.rb
@@ -2,10 +2,10 @@ describe ActivePStore::Inheritance do
   include_context 'Rock stars on stage'
 
   describe 'ActivePStore::Base.new' do
+    let(:error_message) { 'ActivePStore::Base is an abstract class and cannot be instantiated.' }
+
     subject { ActivePStore::Base.new }
 
-    it {
-      expect { subject }.to raise_error(NotImplementedError, 'ActivePStore::Base is an abstract class and cannot be instantiated.')
-    }
+    it { expect { subject }.to raise_error(NotImplementedError, error_message) }
   end
 end
