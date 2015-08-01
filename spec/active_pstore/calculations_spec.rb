e044eb6bfe94564c328888007fce226a6a98250a
Koichi ITO <koic.ito@gmail.com>
2015-08-02 00:49:18 +0900

    add spec

--- spec/active_pstore/calculations_spec.rb
+++ spec/active_pstore/calculations_spec.rb
@@ -10,8 +10,16 @@ describe ActivePStore::Calculations do
   end
 
   describe 'ActivePStore::Collection#ids' do
-    subject { Artist.all.ids }
+    context 'all artists' do
+      subject { Artist.all.ids }
 
-    it { is_expected.to eq all_artist_ids }
+      it { is_expected.to eq all_artist_ids }
+    end
+
+    context 'only guitarists' do
+      subject { Artist.where(instrument: 'guitar').ids }
+
+      it { is_expected.to eq [randy_rhoads, michael_amott, zakk_wylde].map(&:id) }
+    end
   end
 end
