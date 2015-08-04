cebc12eb24ebb1367d84dd57f1d0d3e670b08aaa
Koichi ITO <koic.ito@gmail.com>
2015-08-04 09:55:51 +0900

    add spec for dynamic matchers

--- spec/active_pstore/dynamic_matchers_spec.rb
+++ spec/active_pstore/dynamic_matchers_spec.rb
@@ -35,6 +35,31 @@ describe ActivePStore::DynamicMatchers do
         it { is_expected.to be_nil }
       end
     end
+
+    describe '.find_by_associated_act_and_instrument' do
+      subject { Artist.find_by_associated_act_and_instrument(associated_act, instrument) }
+
+      context 'exist Ozzy Osbourne band keyboardist' do
+        let(:associated_act) { 'Ozzy Osbourne' }
+        let(:instrument) { 'keyboard' }
+
+        it { is_expected.to eq(don_airey) }
+      end
+
+      context 'exist Ozzy Osbourne guitarist' do
+        let(:associated_act) { 'Ozzy Osbourne' }
+        let(:instrument) { 'guitar' }
+
+        it { is_expected.to eq(randy_rhoads) }
+      end
+
+      context 'not found' do
+        let(:associated_act) { 'Led Zeppelin' }
+        let(:instrument) { 'guitar' }
+
+        it { is_expected.to be_nil }
+      end
+    end
   end
 
   describe ActivePStore::DynamicMatchers::FindByBang do
