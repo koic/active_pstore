a5f5b466276fcdd9a1a3e0a3ec87466e703dd739
Koichi ITO <koic.ito@gmail.com>
2015-08-24 00:13:16 +0900

    add spec

--- spec/active_pstore/core_spec.rb
+++ spec/active_pstore/core_spec.rb
@@ -1,9 +1,17 @@
 describe ActivePStore::Core do
   include_context 'Rock stars on stage'
 
-  describe '#==' do
-    subject { Artist.find(randy_rhoads) }
+  let(:randy) { Artist.find(randy_rhoads) }
 
-    it { is_expected.to eq randy_rhoads }
+  describe '#==' do
+    subject { randy == randy_rhoads }
+
+    it { is_expected.to be true }
+  end
+
+  describe '#hash' do
+    subject { randy.hash }
+
+    it { is_expected.to eq randy_rhoads.id.hash }
   end
 end
