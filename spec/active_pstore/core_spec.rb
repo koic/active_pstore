a785db2ba9a524377e7592787f888af206c33bb6
Koichi ITO <koic.ito@gmail.com>
2015-08-23 23:30:50 +0900

    an unused code

--- spec/active_pstore/core_spec.rb
+++ spec/active_pstore/core_spec.rb
@@ -2,8 +2,6 @@ describe ActivePStore::Core do
   include_context 'Rock stars on stage'
 
   describe '#==' do
-    let(:let) { randy_rhoads }
-
     subject { Artist.find(randy_rhoads) }
 
     it { is_expected.to eq randy_rhoads }
