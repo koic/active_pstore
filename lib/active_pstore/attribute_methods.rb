8c916c70133c651d2fee090ce5db67353907bbc1
Koichi ITO <koic.ito@gmail.com>
2015-08-31 20:48:32 +0900

    add ActivePStore::Base#[]= method

--- lib/active_pstore/attribute_methods.rb
+++ lib/active_pstore/attribute_methods.rb
@@ -7,5 +7,13 @@ module ActivePStore
         raise "undefined method `#{attr}'"
       end
     end
+
+    def []=(attr, value)
+      if respond_to? "#{attr}=".to_sym
+        self.__send__("#{attr}=".to_sym, value)
+      else
+        raise "undefined method `#{attr}='"
+      end
+    end
   end
 end
--- spec/active_pstore/attribute_methods_spec.rb
+++ spec/active_pstore/attribute_methods_spec.rb
@@ -22,4 +22,20 @@ describe ActivePStore::AttributeMethods do
       it { expect { subject }.to raise_error(StandardError, "undefined method `undefined_method'") }
     end
   end
+
+  describe '#[]=' do
+    subject { randy_rhoads[attr_name] = 'RR' }
+
+    context 'set name' do
+      let(:attr_name) { :name }
+
+      it { is_expected.to eq randy_rhoads.name }
+    end
+
+    context 'undefined method' do
+      let(:attr_name) { :undefined_method }
+
+      it { expect { subject }.to raise_error(StandardError, "undefined method `undefined_method='") }
+    end
+  end
 end
