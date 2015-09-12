5b6a42c0d658809f21a5f6d10ff095dbb32576c9
Koichi ITO <koic.ito@gmail.com>
2015-09-13 02:03:07 +0900

    add ConfigGenerator

---
+++ lib/rails/generators/active_pstore/config/config_generator.rb
@@ -0,0 +1,15 @@
+module ActivePstore
+  module Generators
+    class ConfigGenerator < Rails::Generators::Base
+      desc 'Creates a Active PStore configuration file at config/active_pstore.yml'
+
+      def self.source_root
+        File.expand_path('../templates', __FILE__)
+      end
+
+      def create_config_file
+        template 'active_pstore.yml', File.join('config', 'active_pstore.yml')
+      end
+    end
+  end
+end
