b47a55dfc1bd7c508b43fb7d0f69a9f011e07c98
Koichi ITO <koic.ito@gmail.com>
2015-09-14 00:16:56 +0900

    add ModelGenerator

---
+++ lib/rails/generators/active_pstore/model/model_generator.rb
@@ -0,0 +1,16 @@
+module ActivePstore
+  module Generators
+    class ModelGenerator < Rails::Generators::NamedBase
+      desc 'Creates a Active PStore model'
+      argument :attributes, type: :array, default: [], banner: 'attribute_name_1 attribute_name_2'
+
+      def self.source_root
+        File.expand_path('../templates', __FILE__)
+      end
+
+      def create_model_file
+        template 'model.rb.tt', File.join('app/models', class_path, "#{file_name}.rb")
+      end
+    end
+  end
+end
