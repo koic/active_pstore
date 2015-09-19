a0af774188d69c126933f516c80c32fef89cfd5b
Koichi ITO <koic.ito@gmail.com>
2015-09-20 06:37:32 +0900

    Extract Class
    http://refactoring.com/catalog/extractClass.html

---
+++ lib/active_pstore/application/configuration.rb
@@ -0,0 +1,27 @@
+module ActivePStore
+  module Application
+    class Configuration
+      # Loads and returns the entire raw configuration of database from
+      # values stored in `config/active_pstore.yml`.
+      def database_configuration
+        yaml = Pathname.new('config/active_pstore.yml')
+
+        config = if yaml && yaml.exist?
+          require 'yaml'
+          require 'erb'
+          YAML.load(ERB.new(yaml.read).result) || {}
+        else
+          raise 'Could not load database configuration. No such file - config/active_pstore.yml'
+        end
+
+        config
+      rescue Psych::SyntaxError => e
+        raise "YAML syntax error occurred while parsing config/active_pstore.yml. " \
+              "Please note that YAML must be consistently indented using spaces. Tabs are not allowed. " \
+              "Error: #{e.message}"
+      rescue => e
+        raise e, "Cannot load `Rails::ActivePStore::Railtie.database_configuration`:\n#{e.message}", e.backtrace
+      end
+    end
+  end
+end
--- lib/active_pstore/railtie.rb
+++ lib/active_pstore/railtie.rb
@@ -11,33 +11,15 @@ module Rails
         config.action_dispatch.rescue_responses.merge!(rescue_responses)
       end
 
-      # Loads and returns the entire raw configuration of database from
-      # values stored in `config/active_pstore.yml`.
-      def database_configuration
-        yaml = Pathname.new('config/active_pstore.yml')
-
-        config = if yaml && yaml.exist?
-          require 'yaml'
-          require 'erb'
-          YAML.load(ERB.new(yaml.read).result) || {}
-        else
-          raise 'Could not load database configuration. No such file - config/active_pstore.yml'
-        end
-
-        config
-      rescue Psych::SyntaxError => e
-        raise "YAML syntax error occurred while parsing config/active_pstore.yml. " \
-              "Please note that YAML must be consistently indented using spaces. Tabs are not allowed. " \
-              "Error: #{e.message}"
-      rescue => e
-        raise e, "Cannot load `Rails::ActivePStore::Railtie.database_configuration`:\n#{e.message}", e.backtrace
-      end
-
       initializer 'active_pstore.initialize_database' do
         config_file = Rails.root.join('config', 'active_pstore.yml')
 
         if config_file.file?
-          database = database_configuration.fetch(ENV['RAILS_ENV'] || 'development').fetch('database')
+          require 'active_pstore/application/configuration'
+
+          configuration = ::ActivePStore::Application::Configuration.new
+
+          database = configuration.database_configuration.fetch(ENV['RAILS_ENV'] || 'development').fetch('database')
 
           ::ActivePStore::Base.establish_connection(database: database)
         end
