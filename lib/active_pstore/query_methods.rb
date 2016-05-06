a5820829357d9722b1707e02214b4fb91b95051e
Koichi ITO <koic.ito@gmail.com>
2016-05-07 05:33:59 +0900

    DRY

--- lib/active_pstore/query_methods.rb
+++ lib/active_pstore/query_methods.rb
@@ -6,10 +6,22 @@ module ActivePStore
       end
 
       def not(conditions)
+        WhereDecider.new(@scope).decide(conditions, :reject)
+      end
+    end
+
+    class WhereDecider
+      def initialize(scope)
+        @scope = scope
+      end
+
+      def decide(conditions, method)
+        raise 'Illegal method error.' unless %i(reject select).include? method
+
         return @scope if conditions.empty?
 
         conditions.each do |key, value|
-          @scope = @scope.reject {|obj|
+          @scope = @scope.public_send(method) {|obj|
             case value
             when Array, Range
               value.include?(obj.public_send(key))
@@ -29,20 +41,7 @@ module ActivePStore
       if conditions == :chain
         WhereChain.new(ret)
       else
-        return ret if conditions.empty?
-
-        conditions.each do |key, value|
-          ret = ret.select {|obj|
-            case value
-            when Array, Range
-              value.include?(obj.public_send(key))
-            else
-              obj.public_send(key) == value
-            end
-          }
-        end
-
-        ActivePStore::Collection.new(ret)
+        WhereDecider.new(ret).decide(conditions, :select)
       end
     end
   end
